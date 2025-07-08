324a9418157c4142f6cf767722f80981071fbb9b
Deebz <projects@deebz.dev>
2025-07-08 09:49:28 +0200

    Add initializeRoutes function

--- App/App.php
+++ App/App.php
@@ -9,5 +9,12 @@ class App
     public function __construct(Framework $framework)
     {
         $framework->run();
+        $this->initializeRoutes();
+    }
+
+    public function initializeRoutes(): void
+    {
+        // Load routes from the routes file
+        require_once __DIR__ . '/routes.php';
     }
 }
