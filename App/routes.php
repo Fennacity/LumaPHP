508d39c320be0bb16add9a0b837272bd1e7a5816
Fennacity <info@dylanbeurskens.nl>
2025-10-13 18:04:56 +0200

    Remove unneccessary code, add head block and add scripts, styles

--- App/routes.php
+++ App/routes.php
@@ -6,8 +6,5 @@ $routes = new Routes();
 
 // Define routes
 $routes->addRoute('GET', '/', 'App\Controllers\HomeController@index');
-$routes->addRoute('GET', '/post', 'App\Controllers\PostController@index');
-$routes->addRoute('GET', '/post/{id}', 'App\Controllers\PostController@show');
-$routes->addRoute('GET', '/test', 'App\Controllers\TestController@index');
 
 return $routes;
