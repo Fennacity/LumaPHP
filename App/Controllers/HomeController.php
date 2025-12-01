b54ae2e12f2dd9ffa3105615201fae49404ca90a
Fennacity <info@dylanbeurskens.nl>
2025-12-01 09:48:40 +0100

    first commit

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController implements ControllerInterface
     public function index() : string
     {
         return Template::render('/home/index.html.twig', [
+            'project_name' => substr_replace($_ENV['PROJECT_NAME'], ' ', 5, 1),
             'title' => 'Welcome to the Home Page',
             'message' => 'This is the home page of our application.'
         ]);
